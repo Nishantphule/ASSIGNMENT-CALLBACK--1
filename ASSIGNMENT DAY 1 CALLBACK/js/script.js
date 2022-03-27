2318bf8c3b1d097d7919fdb517dfddc5089a294a
Nishant Phule <98202193+Nishantphule@users.noreply.github.com>
2022-03-27 14:17:36 +0530

    Add files via upload

---
+++ ASSIGNMENT DAY 1 CALLBACK/js/script.js
@@ -0,0 +1,55 @@
+
+function Message(info) {  
+    document.getElementById('count').innerHTML= "Happy Independence Day !"
+}
+setTimeout(Message, 11000);
+
+function count10(){
+    document.getElementById('count').innerHTML= "10"
+}
+setTimeout(count10, 1000);
+
+function count9(){
+    document.getElementById('count').innerHTML= "9"
+}
+setTimeout(count9, 2000);
+
+function count8(){
+    document.getElementById('count').innerHTML= "8"
+}
+setTimeout(count8, 3000);
+
+function count7(){
+    document.getElementById('count').innerHTML= "7"
+}
+setTimeout(count7, 4000);
+
+function count6(){
+    document.getElementById('count').innerHTML= "6"
+}
+setTimeout(count6, 5000);
+
+function count5(){
+    document.getElementById('count').innerHTML= "5"
+}
+setTimeout(count5, 6000);
+
+function count4(){
+    document.getElementById('count').innerHTML= "4"
+}
+setTimeout(count4, 7000);
+
+function count3(){
+    document.getElementById('count').innerHTML= "3"
+}
+setTimeout(count3, 8000);
+
+function count2(){
+    document.getElementById('count').innerHTML= "2"
+}
+setTimeout(count2, 9000);
+
+function count1(){
+    document.getElementById('count').innerHTML= "1"
+}
+setTimeout(count1, 10000);
